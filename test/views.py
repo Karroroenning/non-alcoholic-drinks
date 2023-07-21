cd42ebb5f319a9d160b4af35163033d94ab54c38
karolina Rönning <Karroroenning@gmail.com>
2023-07-21 11:19:03 +0000

    cleand up my views import. Had alot of import that i didn't needed

--- test/views.py
+++ test/views.py
@@ -1,22 +1,4 @@
-from django.shortcuts import render, reverse
-from django.shortcuts import (render, get_object_or_404,
-                              reverse, redirect)
-from django.utils.decorators import method_decorator
-from django.contrib.auth.decorators import login_required
-from django.contrib import messages
-from django.contrib.auth.mixins import (LoginRequiredMixin,
-                                        UserPassesTestMixin)
-from django.contrib.messages.views import SuccessMessageMixin
-from django.http import HttpResponseRedirect, HttpResponse
-from django.views.generic import (
-    ListView,
-    CreateView,
-    UpdateView,
-    DeleteView
-)
-
-# Create your views here.
-from django.http.response import HttpResponse
+from django.shortcuts import render
 
 
 def test_page_view(request):
