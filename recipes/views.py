43f83e1a43d61f4979e6b3ed5e50ccc90dc3750c
karolina Rönning <Karroroenning@gmail.com>
2023-07-29 15:31:17 +0000

    fixed the commenting on recepies post, and it must be approved by site owner before it published

--- recipes/views.py
+++ recipes/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 
 from .models import Recipes
+from .forms import CommentForm
 
 
 class RecipesList(generic.ListView):
@@ -27,6 +28,38 @@ class RecipesDetail(View):
             {
                 "recipes": recipes,
                 "comments": comments,
-                "liked": liked
+                "commented": False,
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
+        )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Recipes.objects.filter(status=1)
+        recipes = get_object_or_404(queryset, slug=slug)
+        comments = recipes.comments.filter(approved=True).order_by("-created_on")
+        liked = False
+        if recipes.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.recipes = recipes
+            comment.save()
+        else:
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            "recipes_detail.html",
+            {
+                "recipes": recipes,
+                "comments": comments,
+                "commented": True,
+                "liked": liked,
+                "comment_form": CommentForm()
             },
         )
