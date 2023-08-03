2c395fe3e52938d1706a0e18c2b18dba796e570c
karolina Rönning <Karroroenning@gmail.com>
2023-08-03 12:06:46 +0000

    got the function to add_recipe to work, the url path was wrong

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 
 urlpatterns = [
    path('', views.RecipesList.as_view(), name='recipes-urls'),
+   path('add_recipes/', views.add_recipes, name='add_recipes'),
    path('<slug:slug>/', views.RecipesDetail.as_view(), name='recipes-detail'),
    path('<slug:slug>', views.RecipesLike.as_view(), name='recipes_like'),
-   path('add_recipes/', views.add_recipes, name='add_recipes'),
 ]
