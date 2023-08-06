33059941a259d440ee1e7d8c21247d729668ea65
karolina Rönning <Karroroenning@gmail.com>
2023-08-06 18:03:48 +0000

    debug to False just to see if it works with Heroku

--- noalco4me/settings.py
+++ noalco4me/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['non-alco-4me-427be0bd27b2.herokuapp.com', '8000-karroroenni-nonalcoholi-ak9fqj8cuwe.ws-eu102.gitpod.io']
 
